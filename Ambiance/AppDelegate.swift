49bf97370fc0fc5e06d50d139f991f8aa70e8001
Ryan <rchee@calpoly.edu>
2016-11-10 23:05:03 -0800

    Added Parse/ParseFacebookUtilsV4 to pod file and verified communication with Parse server

--- Ambiance/AppDelegate.swift
+++ Ambiance/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "SXQu86CkKMYI3iuKhJHCQqCGtws3vT3c9eWE9WO2"
+                configuration.clientKey = nil  // set to nil assuming you have not set clientKey
+                configuration.server = "https://codepath-ambiance.herokuapp.com/parse"
+            }))
         return true
     }
 
